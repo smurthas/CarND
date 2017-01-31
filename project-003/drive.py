340dd06cb9ffbb2bc11d04793b02bcdaa22fb691
Simon Murtha Smith <simon@murtha-smith.com>
2017-01-30 21:16:05 -0800

    Down to 0.2 throttle.

--- project-003/drive.py
+++ project-003/drive.py
@@ -59,7 +59,7 @@ def telemetry(sid, data):
     # Feel free to change this.
     steering_angle = float(model.predict(transformed_image_array, batch_size=1))
     # The driving model currently just outputs a constant throttle. Feel free to edit this.
-    throttle = 0.3
+    throttle = 0.2
     #print(steering_angle, throttle)
     send_control(steering_angle, throttle)
 
