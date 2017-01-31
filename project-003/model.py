a8404df1fdfc2b222b1a2fba57f800e597a548c2
Simon Murtha Smith <simon@murtha-smith.com>
2017-01-30 20:22:35 -0800

    Output json arch, more comments.

--- project-003/model.py
+++ project-003/model.py
@@ -13,6 +13,7 @@ from keras.layers.convolutional import Convolution2D
 
 
 def build_model():
+    """ Creates a CNN inspired by NVIDIA's DAVE 2 """
     # Create the Sequential model
     model = Sequential()
 
@@ -46,12 +47,18 @@ def build_model():
 
 
 def train(X_train, y_train, model_file, nb_epoch=10):
+    """ Creates an instance of the DAVE 2 model and trains it on the X and y
+    training sets provided for `nb_epoch` epochs. Saves the .h5 model weights to
+    `model_file"""
     model = build_model()
+    with open(model_file + '.json', 'w') as model_json:
+        model_json.write(model.to_json())
     model.compile('adam', 'mse', ['mean_absolute_error'])
     model.fit(X_train, y_train, batch_size=128, nb_epoch=nb_epoch, validation_split=0.2)
-    model.save(model_file)
+    model.save(model_file + '.h5')
 
 def load_training_datas(filenames):
+    """ Loads multiple pickled data files and concatenates them together """
     X_train = []
     y_train = []
     for filename in filenames:
@@ -60,12 +67,14 @@ def load_training_datas(filenames):
             tmp_data = pickle.load(f)
             X_train.extend(tmp_data['features'])
             y_train.extend(tmp_data['angles'])
+
+    # Shuffle the data before returning
     return shuffle(X_train, y_train)
 
-
-if __name__ == '__main__':
+def run():
+    """ Load data, train the model, save the weights """
     if len(sys.argv) < 3:
-        print("usage: sms_model.py [NB_EPOCHS] [OUTFILE NAME] [INPUT FILES]")
+        print("usage: model.py [NB_EPOCHS] [OUTFILE NAME] [INPUT FILES]")
         sys.exit()
 
     nb_epoch = int(sys.argv[1])
@@ -76,3 +85,6 @@ if __name__ == '__main__':
     X_train = np.reshape(np.array(X_train), (-1, 66, 200, 3))
     train(X_train, y_train, model_file_name, nb_epoch=nb_epoch)
 
+
+if __name__ == '__main__':
+    run()
